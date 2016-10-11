9e3dc373e8c462f86f1be31cb4d68a96acd4a8fd
Trevor Hamilton <tjh1003@sru.edu>
2016-10-10 21:41:44 -0400

    Fully implemented color variationer
    Note colors have not been tuned in yet and are still not dynamic for
    extra stuff that will come soon. Currently doing some tests on network
    and processing combined meters to test it out. I may delay going fully
    dynamic until I can fix CPU woes.
    
    Also forgot to mention in last update two things:
    1. CPU usage is high again
    2. I think I have a method to fix my woes around CPU usage, basically I
    am going to have dynamic variables only be on when a song changes, also
    it turns out you can run a command when a measure changes
    (OnChangeAction)! No idea if it will work with music info but it is
    promising.

--- colorVariationer.lua
+++ colorVariationer.lua
@@ -1,34 +1,36 @@
 function varationize(colorR, colorG, colorB, modifyPercent)
-
-
-	print("Color:" .. colorR .. "," .. colorG .. "," .. colorB)
-	colorR = colorR * modifyPercent
-	colorG = colorG * modifyPercent
-	colorB = colorB * modifyPercent
-	
-	local newColorRGB = colorR .. "," .. colorG .. "," .. colorB
-	print("Variation:" .. newColorRGB)
-	return (newColorRGB)
+	print("Modify:" .. modifyPercent)
+	modifyPercent = tonumber(modifyPercent)
+	if (modifyPercent == -1)
+		then
+			print("Color Denied")
+			return("-1")
+		else
+			print("Color:" .. colorR .. "," .. colorG .. "," .. colorB)
+			colorR = colorR * modifyPercent
+			colorG = colorG * modifyPercent
+			colorB = colorB * modifyPercent
+			
+			local newColorRGB = colorR .. "," .. colorG .. "," .. colorB
+			return (newColorRGB)
+	end
 end
 
 function varationizer(baseColorR, baseColorG, baseColorB)
 
+	local baseColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable("ColorModifier", '1.0'))
+	local secondaryColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable("Color2Modifier", '1.0'))
+	local wallpaperColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable("WallpaperColorModifier", '1.0'))
+	local backgroundColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable("BackgroundColorModifier", '1.0'))
+	local transBackgroundColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable("BackgroundNonColorModifier", '1.0'))
+	local textColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable("TextColorModifier", '1.0'))
+	local clockColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable("ClockColorModifier", '1.0'))
 	
-
-	local baseColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable(ColorModifier, '1.0'))
-	print("BaseColor:" .. baseColor)
-	local secondaryColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable(Color2Modifier, '1.0'))
-	local wallpaperColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable(WallpaperColorModifier, '1.0'))
-	local backgroundColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable(BackgroundColor, '1.0'))
-	local transBackgroundColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable(BackgroundNonColorModifier, '1.0'))
-	local textColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable(TextColorModifier, '1.0'))
-	local clockColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable(ClockColorModifier, '1.0'))
-	
-	SKIN:Bang('!SetVariable', "Color", baseColor)
-	SKIN:Bang('!SetVariable', "Color2", secondaryColor)
-	SKIN:Bang('!SetVariable', "WallpaperColor", wallpaperColor)
-	SKIN:Bang('!SetVariable', "BackgroundColor", backgroundColor)
-	SKIN:Bang('!SetVariable', "BackgroundNonColor", transBackgroundColor .. SKIN:GetVariable(BackgroundNonColorTransparency, '255'))
-	SKIN:Bang('!SetVariable', "TextColor", textColor)
-	SKIN:Bang('!SetVariable', "ClockRing", clockColor .. SKIN:GetVariable(ClockRingTransparency, '255'))
+	if baseColor ~= -1 then SKIN:Bang('!SetVariable', "Color", baseColor) end
+	if secondaryColor ~= -1 then SKIN:Bang('!SetVariable', "Color2", secondaryColor) end
+	if wallpaperColor ~= -1 then SKIN:Bang('!SetVariable', "WallpaperColor", wallpaperColor) end
+	if backgroundColor ~= -1 then SKIN:Bang('!SetVariable', "BackgroundColor", backgroundColor) end
+	if transBackgroundColor ~= -1 then SKIN:Bang('!SetVariable', "BackgroundNonColor", transBackgroundColor .. SKIN:GetVariable(BackgroundNonColorTransparency, '255')) end
+	if textColor ~= -1 then SKIN:Bang('!SetVariable', "TextColor", textColor) end
+	if clockColor ~= -1 then SKIN:Bang('!SetVariable', "ClockRing", clockColor .. SKIN:GetVariable(ClockRingTransparency, '255')) end
 end
