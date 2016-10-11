00d05d99443fe7c731f4f983987b6a06882092fb
Trevor Hamilton <tjh1003@sru.edu>
2016-10-10 20:49:49 -0400

    Completed color varationer
    Yes that is a word I just made it up.
    
    Currently only color and color2 have been tested, no widgets are using
    new info besides part of the processing combined meter and colors likely
    dont match yet. Also does not have -1 keeps default value functionality
    yet. Also no idea if transparency stuff works yet.
    
    BUT it works so far just need some testing and simple changes

--- colorVariationer.lua
+++ colorVariationer.lua
@@ -1,12 +1,34 @@
 function varationize(colorR, colorG, colorB, modifyPercent)
-	print("CurrRGB" .. colorR .. colorG .. colorB)
+
+
+	print("Color:" .. colorR .. "," .. colorG .. "," .. colorB)
 	colorR = colorR * modifyPercent
 	colorG = colorG * modifyPercent
 	colorB = colorB * modifyPercent
 	
 	local newColorRGB = colorR .. "," .. colorG .. "," .. colorB
-	
-	print("newColorRGB" .. newColorRGB)
+	print("Variation:" .. newColorRGB)
+	return (newColorRGB)
+end
 
-	SKIN:Bang('!SetVariable', "Color2", newColorRGB)
+function varationizer(baseColorR, baseColorG, baseColorB)
+
+	
+
+	local baseColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable(ColorModifier, '1.0'))
+	print("BaseColor:" .. baseColor)
+	local secondaryColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable(Color2Modifier, '1.0'))
+	local wallpaperColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable(WallpaperColorModifier, '1.0'))
+	local backgroundColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable(BackgroundColor, '1.0'))
+	local transBackgroundColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable(BackgroundNonColorModifier, '1.0'))
+	local textColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable(TextColorModifier, '1.0'))
+	local clockColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable(ClockColorModifier, '1.0'))
+	
+	SKIN:Bang('!SetVariable', "Color", baseColor)
+	SKIN:Bang('!SetVariable', "Color2", secondaryColor)
+	SKIN:Bang('!SetVariable', "WallpaperColor", wallpaperColor)
+	SKIN:Bang('!SetVariable', "BackgroundColor", backgroundColor)
+	SKIN:Bang('!SetVariable', "BackgroundNonColor", transBackgroundColor .. SKIN:GetVariable(BackgroundNonColorTransparency, '255'))
+	SKIN:Bang('!SetVariable', "TextColor", textColor)
+	SKIN:Bang('!SetVariable', "ClockRing", clockColor .. SKIN:GetVariable(ClockRingTransparency, '255'))
 end
