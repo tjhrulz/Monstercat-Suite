81c2f7a4fd2baa995b2b0d7c61ba50a9451507fb
Trevor Hamilton <tjh1003@sru.edu>
2016-10-10 23:14:41 -0400

    Dynamic color load now only loads on song change
    GREATLY reduced CPU usage, now in my test environment with every single
    meter including duplicates running its only about 15% which is only
    about 2% more than before dynamic varriables were turned on. Debating if
    I want to do dynamic varriables dynamically now or once the skins is
    done.

--- colorVariationer.lua
+++ colorVariationer.lua
@@ -1,12 +1,46 @@
+function colorSelector(genre)
+	if string.find(genre, "Electro") then
+		--print("test:" .. SKIN:GetVariable("Electro"))
+		varationizer(SKIN:GetVariable("Electro"))
+	elseif string.find(genre, "EDM") or string.find(genre, "Electronic") or string.find(genre, "Breaks") or string.find(genre, "Chillout") or string.find(genre, "Bounce") or string.find(genre, "Chill") then
+		varationizer(SKIN:GetVariable("EDM"))
+	elseif string.find(genre, "House") or string.find(genre, "Electro House") or string.find(genre, "Progressive House") then
+		varationizer(SKIN:GetVariable("House"))
+	elseif string.find(genre, "Drum & Bass") or string.find(genre, "DnB") then
+		varationizer(SKIN:GetVariable("DnB"))
+	elseif string.find(genre, "Dubstep") then
+		varationizer(SKIN:GetVariable("Dubstep"))
+	elseif string.find(genre, "Drumstep") then
+		varationizer(SKIN:GetVariable("Drumstep"))
+	elseif string.find(genre, "Glitch Hop") or string.find(genre, "GlitchHop") then
+		varationizer(SKIN:GetVariable("GlitchHop"))
+	elseif string.find(genre, "Trap") then
+		varationizer(SKIN:GetVariable("Trap"))
+	elseif string.find(genre, "Trance") or string.find(genre, "Deep House") then
+		varationizer(SKIN:GetVariable("Trance"))
+	elseif string.find(genre, "Hard Dance") then
+	varationizer(SKIN:GetVariable("HardDance"))
+	elseif string.find(genre, "Nu Disco") or string.find(genre, "NuDisco") or string.find(genre, "Disco") or string.find(genre, "Indie Dance") or string.find(genre, "Electro Swing") then
+		varationizer(SKIN:GetVariable("NuDisco"))
+	elseif string.find(genre, "Future") or string.find(genre, "Future Bass") then
+	--print("test:" .. SKIN:GetVariable("Electro"))
+		varationizer(SKIN:GetVariable("FutureBass"))
+	elseif string.find(genre, "Mashup") or string.find(genre, "Mash Up") then
+		varationizer(SKIN:GetVariable("Mashup"))
+	else
+		varationizer(SKIN:GetVariable(DefaultDynamicColor))
+	end
+end
+
 function varationize(colorR, colorG, colorB, modifyPercent)
-	print("Modify:" .. modifyPercent)
+	--print("Modify:" .. modifyPercent)
 	modifyPercent = tonumber(modifyPercent)
 	if (modifyPercent == -1)
 		then
-			print("Color Denied")
+			--print("Color Denied")
 			return("-1")
 		else
-			print("Color:" .. colorR .. "," .. colorG .. "," .. colorB)
+			--print("Color:" .. colorR .. "," .. colorG .. "," .. colorB)
 			colorR = colorR * modifyPercent
 			colorG = colorG * modifyPercent
 			colorB = colorB * modifyPercent
@@ -16,7 +50,13 @@ function varationize(colorR, colorG, colorB, modifyPercent)
 	end
 end
 
-function varationizer(baseColorR, baseColorG, baseColorB)
+function varationizer(baseColorRGB)
+
+	--print("baseColorRGB:" .. baseColorRGB)
+	
+	local baseColorR = tonumber(string.sub(baseColorRGB, 0, 3))
+	local baseColorG = tonumber(string.sub(baseColorRGB, 5, 7))
+	local baseColorB = tonumber(string.sub(baseColorRGB, 9, 11))
 
 	local baseColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable("ColorModifier", '1.0'))
 	local secondaryColor = varationize(baseColorR, baseColorG, baseColorB, SKIN:GetVariable("Color2Modifier", '1.0'))
