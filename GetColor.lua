7e590a0c25e0ebb87f6194b9c5e3fa9d50048d17
Trevor Hamilton <tjh1003@sru.edu>
2016-10-27 12:16:51 -0400

    Attempts to fix leftover threads
    I have attempted to solve the issue with leftover threads by adding two
    things.
    1. A timeout that is the same amount of time as the delay however this
    timeout seems to be doing nothing....
    2. A check to see if the program is still running and if it is kill it,
    however it would appear to sometimes not kill it and other times try to
    kill it when it is not running.

--- GetColor.lua
+++ GetColor.lua
@@ -4,80 +4,59 @@ function GetColors(doPalette)
 			ImagePath = SKIN:GetMeasure('GetImagePath'):GetOption('Text')
 			vbsPath = SKIN:GetMeasure('CalcRootFilePath'):GetOption('Text')
 
-			--print(SKIN:MakePathAbsolute(' ') .. " AlbumArtLoc " .. ImagePath)
-
 			if (ImagePath == nil) or (string.len(ImagePath) <= 1) then
 				FallbackPath = vbsPath .. "@Resources\\images\\output.txt"
 				
 				cmdCommand = "more " .. FallbackPath
 				
 				SKIN:Bang('!SetOption', 'CopyAverageColor', 'Parameter', cmdCommand)
-				--SKIN:Bang('!CommandMeasure', 'CopyAverageColor', 'Kill')
-				--SKIN:Bang('!CommandMeasure', 'RunAverageColor', 'Kill')
+				KillAllRunning()
+				
 				SKIN:Bang('!CommandMeasure', 'CopyAverageColor', 'Run')
-				
-				--baseColorRGB = "200,200,200"
-				--print("Need to implement fallback for no album art " .. cmdCommand)
 			else
-				
-				--SKIN:Bang('!CommandMeasure ' .. ImagePath .. ' "Kill"')
-				
-				--command = vbsPath .. "ImagePath.bat " .. vbsPath .. " " .. ImagePath .. " " .. vbsPath
+
 				-- -colors 16 -depth 8 -format "%c" histogram:info: | sort /r
 				-- -scale 1x1! -format %[fx:int(255*r+.5)],%[fx:int(255*g+.5)],%[fx:int(255*b+.5)] info:
 				cmdCommand = vbsPath .. "@Resources\\ImageMagickScripts\\convert.exe " .. ImagePath .. "  -colors 4 -depth 8 -format %c histogram:info: | sort /r"
-				--cmdCommand = "echo test"
 			
 				SKIN:Bang('!SetOption', 'RunAverageColor', 'Parameter', cmdCommand)
+				KillAllRunning()
 				
-				--print("IP" .. ImagePath)
-				
-				--print(cmdCommand)
-				--print(SKIN:GetMeasure('RunAverageColor'):GetOption('Parameter'))
-				--SKIN:Bang('!CommandMeasure', 'RunAverageColor', 'Kill')
-				--SKIN:Bang('!CommandMeasure', 'CopyAverageColor', 'Kill')
 				SKIN:Bang('!CommandMeasure', 'RunAverageColor', 'Run')
-				--print(SKIN:Bang(cmdCommand))
 			end
 		else
 			ImagePath = SKIN:GetMeasure('GetImagePath'):GetOption('Text')
 			vbsPath = SKIN:GetMeasure('CalcRootFilePath'):GetOption('Text')
 
-			--print(SKIN:MakePathAbsolute(' ') .. " AlbumArtLoc " .. ImagePath)
-
 			if (ImagePath == nil) or (string.len(ImagePath) <= 1) then
 				FallbackPath = vbsPath .. "@Resources\\images\\output.txt"
 				
 				cmdCommand = "more " .. FallbackPath
 				
 				SKIN:Bang('!SetOption', 'CopyAverageColor', 'Parameter', cmdCommand)
-				--SKIN:Bang('!CommandMeasure', 'CopyAverageColor', 'Kill')
-				--SKIN:Bang('!CommandMeasure', 'RunAverageColor', 'Kill')
+				KillAllRunning()
+				
 				SKIN:Bang('!CommandMeasure', 'CopyAverageColor', 'Run')
-				
-				--baseColorRGB = "200,200,200"
-				--print("Need to implement fallback for no album art " .. cmdCommand)
 			else
-				
-				--SKIN:Bang('!CommandMeasure ' .. ImagePath .. ' "Kill"')
-				
-				--command = vbsPath .. "ImagePath.bat " .. vbsPath .. " " .. ImagePath .. " " .. vbsPath
+
 				-- -colors 16 -depth 8 -format "%c" histogram:info: | sort /r
 				-- -scale 1x1! -format %[fx:int(255*r+.5)],%[fx:int(255*g+.5)],%[fx:int(255*b+.5)] info:
 				cmdCommand = vbsPath .. "@Resources\\ImageMagickScripts\\convert.exe " .. ImagePath .. "  -colors 4 -depth 8 -format %c histogram:info: | sort /r"
-				--cmdCommand = "echo test"
 			
 				SKIN:Bang('!SetOption', 'RunAverageColor', 'Parameter', cmdCommand)
+				KillAllRunning()
 				
-				--print("IP" .. ImagePath)
-				
-				--print(cmdCommand)
-				--print(SKIN:GetMeasure('RunAverageColor'):GetOption('Parameter'))
-				--SKIN:Bang('!CommandMeasure', 'RunAverageColor', 'Kill')
-				--SKIN:Bang('!CommandMeasure', 'CopyAverageColor', 'Kill')
 				SKIN:Bang('!CommandMeasure', 'RunAverageColor', 'Run')
-				--print(SKIN:Bang(cmdCommand))
 			end
 		end
+	end
+end
+
+function KillAllRunning()
+	if(SKIN:GetMeasure('RunAverageColor'):GetValue() == 0) then
+		SKIN:Bang('!CommandMeasure', 'RunAverageColor', 'Kill')
+	end
+	if(SKIN:GetMeasure('CopyAverageColor'):GetValue() == 0) then
+		SKIN:Bang('!CommandMeasure', 'CopyAverageColor', 'Kill')
 	end
 end
