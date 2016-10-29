b7d0e99e111c58acdd0e5f477dd5627b60374849
Trevor Hamilton <tjh1003@sru.edu>
2016-10-28 20:37:51 -0400

    Fixes to the album blur system
    It should now work in every case except the first pass that it runs just
    like the album color extractor. Also I still need to find a way to
    update them when iTunes closes I think.

--- @Resources/scripts/albumArtBlur.lua
+++ @Resources/scripts/albumArtBlur.lua
@@ -8,24 +8,26 @@ function blur(inputPath, outputPath)
 	if(inputPath ~= nil) and (string.len(inputPath) >= 1) then		
 		local cmdCommand = rootPath .. "ImageMagickScripts\\convert.exe " .. inputPath .. " -channel RGBA -blur 0x" .. blurAmount .. " " .. outputPath
 
-		SKIN:Bang('!SetOption', 'RunBlurColor', 'Parameter', cmdCommand)
 		if(SKIN:GetMeasure('RunBlurColor'):GetValue() == 0) then
 			--print("Killing RunBlurColor")
 			SKIN:Bang('!CommandMeasure', 'RunBlurColor', 'Kill')
 			--print("RunBlurColor Value:" .. SKIN:GetMeasure('RunBlurColor'):GetValue())
 		end
+		
+		SKIN:Bang('!SetOption', 'RunBlurColor', 'Parameter', cmdCommand)
 		--print("Running normal blur" .. SKIN:GetMeasure('RunBlurColor'):GetOption('Parameter'))
 		SKIN:Bang('!CommandMeasure', 'RunBlurColor', 'Run')
 	else
 		local fallbackPath = SKIN:GetVariable("@") .. "images\\Fallback.png"		
 		local cmdCommand = rootPath .. "ImageMagickScripts\\convert.exe " .. fallbackPath .. " -channel RGBA -blur 0x" .. blurAmount .. " " .. outputPath
-
-		SKIN:Bang('!SetOption', 'RunBlurColor', 'Parameter', cmdCommand)
+		
 		if(SKIN:GetMeasure('RunBlurColor'):GetValue() == 0) then
 			--print("Killing RunBlurColor")
 			SKIN:Bang('!CommandMeasure', 'RunBlurColor', 'Kill')
 			--print("RunBlurColor Value:" .. SKIN:GetMeasure('RunBlurColor'):GetValue())
 		end
+		
+		SKIN:Bang('!SetOption', 'RunBlurColor', 'Parameter', cmdCommand)
 		--print("Running fallback blur" .. SKIN:GetMeasure('RunBlurColor'):GetOption('Parameter'))
 		SKIN:Bang('!CommandMeasure', 'RunBlurColor', 'Run')
 	end
